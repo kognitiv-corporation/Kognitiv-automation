8b2068fc5cf63c2c5468d23a30f27db19f8130d7
Shammika-kognitiv <shammika.dahanayaka@kognitiv.com>
2024-08-07 14:26:45 +0530

    modified pom.xml

--- src/com/testautomation/Utility/BrowserUtility.java
+++ src/com/testautomation/Utility/BrowserUtility.java
@@ -19,8 +19,8 @@ public class BrowserUtility {
 
 			if (browserName.equalsIgnoreCase("Chrome")) {
 				//System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-				//System.setProperty("webdriver.chrome.driver", "resources/drivers/chromedriver.exe");
-				System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver\\chromedriver.exe");
+				System.setProperty("webdriver.chrome.driver", "resources/drivers/chromedriver.exe");
+				//System.setProperty("webdriver.chrome.driver", "C:\\tools\\chromedriver\\chromedriver.exe");
 				ChromeOptions options = new ChromeOptions();
 				options.addArguments("--headless");
 				options.addArguments("--no-sandbox");
